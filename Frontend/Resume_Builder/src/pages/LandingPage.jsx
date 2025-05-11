8f95008924fb56ed023c41d065570eb097c92af1
Titankunal <89270242+Titankunal@users.noreply.github.com>
2025-05-11 20:49:56 +0530

    startfrm0

--- Frontend/Resume_Builder/src/pages/LandingPage.jsx
+++ Frontend/Resume_Builder/src/pages/LandingPage.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { use, useState } from 'react'
+// import image
+import {userNavigate} from 'react-router-dom'
+
 
 const LandingPage = () => {
+  const navigate =userNavigate();
+  
+  const [openAuthModal, setOpenAuthModal] = useState(false);
+  const [currentPage, setCurrentPage] = useState("login");
+
+  const handleCTA = () => {};
   return (
     <div>LandingPage</div>
   )

44f4096b57fb44da1c516ae34bdd6f27c4f84717
Titankunal <89270242+Titankunal@users.noreply.github.com>
2025-05-11 20:59:47 +0530

    start001

--- Frontend/Resume_Builder/src/pages/LandingPage.jsx
+++ Frontend/Resume_Builder/src/pages/LandingPage.jsx
@@ -11,7 +11,19 @@ const LandingPage = () => {
 
   const handleCTA = () => {};
   return (
-    <div>LandingPage</div>
+    <div>
+      <div className='container mx-auto px-4 py-6'>
+        {/* Header */}
+        <header className='flex justify-between items-center mb-16'>
+          <div className='text-xl font-bold'> Resume Builder </div>
+          <button className='bg-purple-100 text-sm font-semibold text-black px-7 py-2.5 rounded-lg hover:text-white transition-colors cursor-pointer'
+            onclick={() => setOpenAuthModal(true)}
+          >
+            Login / Sign Up
+          </button>
+        </header>
+      </div>
+    </div>
   )
 }
 
